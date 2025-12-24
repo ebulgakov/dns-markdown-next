9470d32b0260e0b7adb5d4a63b49f4c4482e6d6d
Eugene Bulgakov <zoom81@yandex.ru>
2025-12-25 00:12:05 +0400

    Add a webhook for saving user in the mongo

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,60 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { dbConnect } from "@/db/database";
+import { User } from "@/db/models/user_model";
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error("NO CLERK_WEBHOOK_SECRET in the env file");
+  }
+
+  const headerPayload = await headers();
+
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400
+    });
+  }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400
+    });
+  }
+
+  const eventType = evt.type;
+
+  if (eventType === "user.created") {
+    await dbConnect();
+    try {
+      await User.create({ userId: evt.data.id });
+    } catch (err) {
+      console.error("Error creation user:", err);
+      throw new Error("Error creation user");
+    }
+  }
+
+  return NextResponse.json({ success: true }, { status: 200 });
+}
