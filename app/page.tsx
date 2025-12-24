afe468cd467ca39825f26422cd79477ec08e939a
Eugene Bulgakov <zoom81@yandex.ru>
2025-12-24 22:28:40 +0400

    Add a test link to the catalog

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       This is a landing page.&nbsp;
-      <Link href="/sign-in">
-        <span className="text-blue-500 hover:underline">Please auth before</span>
-      </Link>
+      <SignedIn>
+        <Link href="/catalog">
+          <span className="text-blue-500 hover:underline">Visit the catalog page</span>
+        </Link>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/sign-in">
+          <span className="text-blue-500 hover:underline">Please auth before</span>
+        </Link>
+      </SignedOut>
     </div>
   );
 }
