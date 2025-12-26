04c98700769d6b07d1ea58d818fdc33489c6aa02
Eugene Bulgakov <zoom81@yandex.ru>
2025-12-26 18:08:20 +0400

    Add links dynamically

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,47 +3,48 @@ import Link from "next/link";
 import ActiveLink from "@/app/components/ActiveLink";
 
 export default function Navbar() {
+  const linksList: {
+    name: string;
+    url: string;
+  }[] = [
+    {
+      name: "Прайслист",
+      url: "/catalog"
+    },
+    {
+      name: "Архив",
+      url: "/archive"
+    },
+    {
+      name: "Обновления",
+      url: "/updates"
+    },
+    {
+      name: "Избранное",
+      url: "/favorites"
+    },
+    {
+      name: "Профиль",
+      url: "/profile"
+    }
+  ];
+
   return (
     <header className="flex h-10 items-center gap-4 bg-blue-400 mt-4">
       <nav className="flex">
         <Link href="/" className="bg-white font-bold text-blue-400 h-10 flex items-center px-3">
           DNS Уценка
         </Link>
-        <ActiveLink
-          href="/catalog"
-          activeClassName="bg-orange-400 text-shadow-xs"
-          className=" text-white h-10 flex items-center px-3"
-        >
-          Прайслист
-        </ActiveLink>
-        <ActiveLink
-          href="/archive"
-          activeClassName="bg-orange-400 text-shadow-xs"
-          className="text-white h-10 flex items-center px-3"
-        >
-          Архив
-        </ActiveLink>
-        <ActiveLink
-          href="/updates"
-          activeClassName="bg-orange-400 text-shadow-xs"
-          className=" text-white h-10 flex items-center px-3"
-        >
-          Обновления
-        </ActiveLink>
-        <ActiveLink
-          href="/favorites"
-          activeClassName="bg-orange-400 text-shadow-xs"
-          className="text-white h-10 flex items-center px-3"
-        >
-          Избранное
-        </ActiveLink>
-        <ActiveLink
-          href="/profile"
-          activeClassName="bg-orange-400 text-shadow-xs"
-          className=" text-white h-10 flex items-center px-3"
-        >
-          Профиль
-        </ActiveLink>
+        {linksList.map(link => (
+          <ActiveLink
+            key={link.name}
+            href={link.url}
+            activeClassName="bg-orange-400 text-shadow-xs"
+            className=" text-white h-10 flex items-center px-3"
+          >
+            {link.name}
+          </ActiveLink>
+        ))}
       </nav>
 
       <div className="ml-auto mr-2 size-7">
