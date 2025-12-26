e39e6333f25762e494ce37eb48c4a178d62518fb
Eugene Bulgakov <zoom81@yandex.ru>
2025-12-26 20:47:21 +0400

    Add profile section

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,24 @@
-export default function ProfilePage() {
+import { getUser } from "@/db/profile/queries";
+import ErrorMessage from "@/app/components/ErrorMessage";
+
+export default async function ProfilePage() {
+  let profile;
+  let error: Error | null = null;
+
+  try {
+    profile = await getUser();
+  } catch (e) {
+    error = e as Error;
+  }
+
+  if (!profile && error) {
+    return <ErrorMessage>{error.message}</ErrorMessage>;
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
-      <p className="text-lg">This is your profile page.</p>
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Профиль</h1>
+      <div className="text-lg">{JSON.stringify(profile, null, 2)}</div>
     </div>
   );
 }
