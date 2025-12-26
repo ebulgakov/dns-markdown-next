9043e58147beebdb8c2d7167745f8115f44865eb
Eugene Bulgakov <zoom81@yandex.ru>
2025-12-26 18:22:19 +0400

    Update signin and signup pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return <div className="min-h-full flex items-center justify-center">
+    <SignUp />
+  </div>;
 }
