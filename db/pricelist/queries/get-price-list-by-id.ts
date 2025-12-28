782a02ccee61131bc5d30f5cec620b8ad283ef36
Eugene Bulgakov <zoom81@yandex.ru>
2025-12-28 10:08:12 +0400

    Update structure for pricelists

---
+++ db/pricelist/queries/get-price-list-by-id.ts
@@ -0,0 +1,8 @@
+import { dbConnect } from "@/db/database";
+import { Pricelist } from "@/db/models/pricelist_model";
+import type { PriceList as PriceListType } from "@/types/pricelist";
+
+export const getPriceListById = async (id: string) => {
+  await dbConnect();
+  return Pricelist.findOne({ _id: id }) as unknown as PriceListType | null;
+};
---
+++ db/pricelist/queries/get-product-by-id.ts
@@ -0,0 +1,33 @@
+import { dbConnect } from "@/db/database";
+import { History } from "@/db/models/history_model";
+import type { History as HistoryType } from "@/types/history";
+import { HourlyPricelist } from "@/db/models/pricelist_model";
+import type { PriceList as PriceListType } from "@/types/pricelist";
+
+export const getProductById = async (id: string) => {
+  await dbConnect();
+
+  const history = (await History.findOne(
+    { link: id },
+    {},
+    { sort: { updatedAt: -1 } }
+  )) as unknown as HistoryType | null;
+  if (!history) return null;
+
+  const priceList = (await HourlyPricelist.findOne(
+    { city: history.city },
+    {},
+    { sort: { updatedAt: -1 } }
+  )) as unknown as PriceListType | null;
+
+  if (!priceList) return null;
+
+  const positions = priceList.toObject().positions.flatMap(positionGroup => positionGroup.items);
+  const item = positions.find((position: { link: string }) => position.link === id);
+
+  if (!item) return null;
+
+  item.city = history.city; // Added city specially for adding to favorites
+
+  return { item, history };
+};
