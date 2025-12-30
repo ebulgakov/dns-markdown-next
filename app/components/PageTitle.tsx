b9e9c6edf5f8bb35b29a27434f7e7015bb34d0f7
Eugene Bulgakov <zoom81@yandex.ru>
2025-12-31 00:33:41 +0400

    Remove sticky from title

--- app/components/PageTitle.tsx
+++ app/components/PageTitle.tsx
@@ -7,9 +7,9 @@ type PageTitleProps = {
 };
 export default function PageTitle({ title, subTitle, children }: PageTitleProps) {
   return (
-    <div className="flex justify-between items-end border-b border-solid border-b-neutral-300  my-5 py-5 sticky top-0 bg-white z-10">
+    <div className="flex justify-between items-end border-b border-solid border-b-neutral-300  my-5 py-5 bg-white ">
       <h1 className="text-4xl">
-        {title}{' '}
+        {title}{" "}
         {subTitle && (
           <small className="font-normal leading-none text-[#777777] text-[65%]">{subTitle}</small>
         )}
--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -7,13 +7,15 @@ export default function SearchInput() {
 
   return (
     <div className="fixed left-0 right-0 bottom-0 z-10 bg-white p-5 border-t border-gray-200">
-      <input
-        type="search"
-        value={searchTerm}
-        className="block w-full rounded-md border-2 border-blue-400 p-2 focus:border-blue-500 disabled:border-blue-300 disabled:bg-blue-50"
-        onChange={e => onChange(e.target.value)}
-        placeholder="Поиск по названию товара..."
-      />
+      <div className="sm:w-[750px] md:w-[970px] lg:w-[1170px] px-4 mx-auto">
+        <input
+          type="search"
+          value={searchTerm}
+          className="block w-full rounded-md border-2 border-blue-400 p-2 focus:border-blue-500 disabled:border-blue-300 disabled:bg-blue-50"
+          onChange={e => onChange(e.target.value)}
+          placeholder="Поиск по названию товара..."
+        />
+      </div>
     </div>
   );
 }
