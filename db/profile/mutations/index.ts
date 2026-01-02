64c5ed3381232ff2a1a1aece4f08111793e0c53c
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-02 22:10:03 +0400

    Remove index mutation file

--- db/profile/mutations/index.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-export { createUser } from "@/db/profile/mutations/create-user";
-export { updateUser } from "@/db/profile/mutations/update-user";
-export { addToFavorites } from "@/db/profile/mutations/add-to-favorites";
-export { removeFromFavorites } from "@/db/profile/mutations/remove-from-favorites";
-export { updateUserSection } from "@/db/profile/mutations/update-user-section";
