36795ce204066e28ea8194311f01ab66022fab72
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-03 23:06:42 +0400

    Fix letter case

--- app/components/__stories__/Alert.stories.tsx
+++ app/components/__stories__/Alert.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Alert from "../Alert";
 
 const meta: Meta<typeof Alert> = {
-  title: "components/Alert",
+  title: "Components/Alert",
   component: Alert,
   tags: ["autodocs"],
   argTypes: {
