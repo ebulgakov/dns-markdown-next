e59b54e6642db31ca2802aba296addd9c5f50fed
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-03 18:00:48 +0400

    Make component type more easy

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,7 @@
-import type { ButtonHTMLAttributes } from "react";
+import type { ComponentProps } from "react";
 import cn from "classnames";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+type ButtonProps = ComponentProps<"button"> & {
   variant?: "primary" | "default";
 };
 export default function Button({ children, variant = "default", ...props }: ButtonProps) {
@@ -11,7 +11,7 @@ export default function Button({ children, variant = "default", ...props }: Butt
         "transition rounded cursor-pointer  px-2 py-1 text-white  disabled:opacity-40",
         {
           "bg-orange-400 hover:bg-orange-500": variant === "primary",
-          "bg-blue-400 hover:bg-blue-400": variant === "default"
+          "bg-blue-400 hover:bg-blue-500": variant === "default"
         }
       )}
       {...props}
