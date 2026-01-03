803345ee9d546e5458d31e588cddc5e01743e084
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-03 21:55:34 +0400

    Add Stories ProductPricesChart

---
+++ app/components/__stories__/ProductPricesChart.stories.tsx
@@ -0,0 +1,70 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ProductPricesChart from "../ProductPricesChart";
+
+const meta: Meta<typeof ProductPricesChart> = {
+  title: "Components/ProductPricesChart",
+  component: ProductPricesChart,
+  tags: ["autodocs"],
+  argTypes: {
+    chartData: {
+      control: "object",
+      description: "Data for the chart"
+    }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof ProductPricesChart>;
+
+const sampleChartData = {
+  labels: [
+    "2023-01-01T00:00:00.000Z",
+    "2023-01-02T00:00:00.000Z",
+    "2023-01-03T00:00:00.000Z",
+    "2023-01-04T00:00:00.000Z",
+    "2023-01-05T00:00:00.000Z"
+  ],
+  price: ["100", "110", "105", "115", "120"],
+  priceOld: ["120", "125", "120", "130", "135"],
+  profit: ["20", "15", "15", "15", "15"]
+};
+
+export const Default: Story = {
+  args: {
+    chartData: sampleChartData
+  }
+};
+
+export const WithNoData: Story = {
+  args: {
+    chartData: null
+  }
+};
+
+export const WithSingleDataPoint: Story = {
+  args: {
+    chartData: {
+      labels: ["2023-01-01T00:00:00.000Z"],
+      price: ["100"],
+      priceOld: ["120"],
+      profit: ["20"]
+    }
+  }
+};
+
+export const WithSomeMissingData: Story = {
+  args: {
+    chartData: {
+      labels: [
+        "2023-01-01T00:00:00.000Z",
+        "2023-01-02T00:00:00.000Z",
+        "-",
+        "2023-01-04T00:00:00.000Z",
+        "2023-01-05T00:00:00.000Z"
+      ],
+      price: ["100", "110", "0", "115", "120"],
+      priceOld: ["120", "125", "0", "130", "135"],
+      profit: ["20", "15", "0", "15", "15"]
+    }
+  }
+};
