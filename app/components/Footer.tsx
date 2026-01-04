13bb378c6ebebcc8d9eafb737f21c5bfebd9ec21
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-04 23:28:05 +0400

    Add Footer i18n

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import ChangeLocationSelector from "@/app/components/ChangeLocationSelector";
+import { useTranslations } from "next-intl";
 
 type FooterProps = {
   locate?: string;
 };
 export default function Footer({ locate }: FooterProps) {
+  const t = useTranslations("Footer");
   return (
     <footer className="mt-auto border-t border-neutral-300 h-13 flex items-center justify-between">
-      <div className="text-sm text-gray-500">© 2018&ndash;2026 DNS-Markdown. Версия 2.0.0.</div>
+      <div className="text-sm text-gray-500">{t("copyright")}</div>
       <div className="text-sm text-gray-500 flex gap-4">
         <ChangeLocationSelector locate={locate} />
         <Link href="/disclaimer">
-          <span className="text-blue-500 hover:underline">Отказ от ответственности</span>
+          <span className="text-blue-500 hover:underline">{t("disclaimer")}</span>
         </Link>
       </div>
     </footer>
--- app/components/__stories__/Footer.stories.tsx
+++ app/components/__stories__/Footer.stories.tsx
@@ -1,12 +1,22 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Footer from "../Footer";
+import { NextIntlClientProvider } from "next-intl";
+import messages from "@/i18n/locates/ru.json";
+import React from "react";
 
 const meta: Meta<typeof Footer> = {
   title: "UI/Footer",
   component: Footer,
   parameters: {
     layout: "fullscreen"
-  }
+  },
+  decorators: [
+    Story => (
+      <NextIntlClientProvider locale="ru" messages={messages}>
+        <Story />
+      </NextIntlClientProvider>
+    )
+  ]
 };
 
 export default meta;
