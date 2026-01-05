1f172bdf03b09d2c886bc78680b34350a739b1e5
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-05 15:06:46 +0400

    Replace moved to mutation

--- db/pricelist/queries/__tests__/get-price-lists-diff.test.ts
+++ db/pricelist/queries/__tests__/get-price-lists-diff.test.ts
@@ -1,7 +1,7 @@
 import { getPriceListsDiff } from "../get-price-lists-diff";
 import { dbConnect } from "@/db/database";
 import { getUser } from "@/db/profile/queries";
-import { RemovedGoods } from "@/db/models/removed_goods_model";
+import { RemovedGoods, NewGoods } from "@/db/models/mutated_goods_model";
 import { Diff } from "@/db/models/diff_model";
 import type { User } from "@/types/user";
 import type { Diff as DiffType } from "@/types/diff";
@@ -16,8 +16,11 @@ jest.mock("@/db/profile/queries", () => ({
   getUser: jest.fn()
 }));
 
-jest.mock("@/db/models/removed_goods_model", () => ({
+jest.mock("@/db/models/mutated_goods_model", () => ({
   RemovedGoods: {
+    findOne: jest.fn()
+  },
+  NewGoods: {
     findOne: jest.fn()
   }
 }));
