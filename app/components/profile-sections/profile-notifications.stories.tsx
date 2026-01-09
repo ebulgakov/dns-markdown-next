7bd486b66976a4b4dd9133161858732e17ed40c2
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-09 12:44:07 +0400

    Add Storybook on Profile Sections

---
+++ app/components/profile-sections/profile-notifications.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ProfileNotifications } from "./profile-notifications";
+
+const meta: Meta<typeof ProfileNotifications> = {
+  title: "Components/Profile/Notifications",
+  component: ProfileNotifications,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen"
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof ProfileNotifications>;
+
+export const Default: Story = {
+  args: {
+    notifications: {
+      updates: {
+        interval: "daily",
+        fields: {
+          new: true,
+          prices: true,
+          profit: false
+        }
+      },
+      favorites: {
+        interval: "weekly"
+      },
+      favoriteSections: {
+        interval: "monthly"
+      }
+    }
+  }
+};
