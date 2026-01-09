2904b33d2435a5f2894fb41469cbb39a97a69b07
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-09 17:57:44 +0400

    Update selector

--- app/components/change-theme-selector/change-theme-selector.tsx
+++ app/components/change-theme-selector/change-theme-selector.tsx
@@ -1,18 +1,29 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import { useTranslations } from "next-intl";
 
 function ChangeThemeSelector() {
   const { theme, setTheme } = useTheme();
+  const [isClient, setIsClient] = useState(false);
+  const t = useTranslations("ChangeThemeSelector");
+  const altTheme = theme === "light" ? "dark" : "light";
 
-  return (
+  useEffect(() => {
+    // https://react.dev/reference/react-dom/client/hydrateRoot#handling-different-client-and-server-content
+    setIsClient(true); // eslint-disable-line react-hooks/set-state-in-effect
+  }, []);
+
+  return isClient ? (
     <button
-      className="cursor-pointer hover:underline"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      suppressHydrationWarning={false}
+      className="cursor-pointer flex items-center"
+      onClick={() => setTheme(altTheme)}
     >
-      Поменять тему сайта
+      {t(altTheme)} {altTheme === "light" ? "🌞" : "🌜"}
     </button>
-  );
+  ) : null;
 }
 
 export { ChangeThemeSelector };
