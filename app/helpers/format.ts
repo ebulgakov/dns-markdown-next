9c134e296d0119e80d5862c8d13884f8503e06a8
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-11 12:20:53 +0400

    Update format dates helper

--- app/helpers/format.ts
+++ app/helpers/format.ts
@@ -1,38 +1,47 @@
-export const formatDate = (date: Date | number | string): string => {
-  const dateFormat = new Intl.DateTimeFormat("ru", {
+type CustomDate = Date | number | string;
+
+const formatGenerator = (options: Intl.DateTimeFormatOptions): ((date: CustomDate) => string) => {
+  return (date: CustomDate): string => {
+    const dateFormat = new Intl.DateTimeFormat("ru", options);
+    return dateFormat.format(new Date(date));
+  };
+};
+
+export const formatDate = (date: CustomDate): string => {
+  const format = formatGenerator({
     year: "numeric",
     month: "long",
     day: "numeric"
   });
 
-  return dateFormat.format(new Date(date));
+  return format(date);
 };
 
-export const formatDateShort = (date: Date | number | string): string => {
-  const dateFormat = new Intl.DateTimeFormat("ru", {
+export const formatDateShort = (date: CustomDate): string => {
+  const format = formatGenerator({
     year: "2-digit",
     month: "2-digit",
     day: "2-digit"
   });
 
-  return dateFormat.format(new Date(date));
+  return format(date);
 };
 
-export const formatDateMonthDay = (date: Date | number | string): string => {
-  const dateFormat = new Intl.DateTimeFormat("ru", {
+export const formatDateMonthDay = (date: CustomDate): string => {
+  const format = formatGenerator({
     month: "short",
     day: "numeric"
   });
 
-  return dateFormat.format(new Date(date));
+  return format(date);
 };
 
 export const formatTime = (date: Date | number | string): string => {
-  const timeFormat = new Intl.DateTimeFormat("ru", {
+  const format = formatGenerator({
     hour: "numeric",
     minute: "numeric",
     second: "numeric"
   });
 
-  return timeFormat.format(new Date(date));
+  return format(date);
 };
