6c66e16450092bb2b30b5f499272720237503591
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-11 10:59:23 +0400

    Add mobile navbar

--- app/components/navbar/navbar-mobile.tsx
+++ app/components/navbar/navbar-mobile.tsx
@@ -1,8 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { Menu } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/app/components/ui/button";
-import { NavigationMenu } from "@/app/components/ui/navigation-menu";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/app/components/ui/dialog";
 
 import type { NavbarLinks } from "@/types/common";
 
@@ -13,17 +21,66 @@ type NavbarMobileProps = {
 
 function NavbarMobile({ t, linksList }: NavbarMobileProps) {
   return (
-    <div className="border-primary flex items-center gap-4 rounded border-2 py-2">
-      <NavigationMenu>
+    <Dialog>
+      <div className="border-primary flex items-center gap-4 rounded border-2 py-2">
         <Button asChild variant="link" className="font-bold">
           <Link href="/">{t("logo")}</Link>
         </Button>
-      </NavigationMenu>
 
-      <Button className="ml-auto" variant="link">
-        <Menu />
-      </Button>
-    </div>
+        <DialogTrigger asChild>
+          <Button className="ml-auto" variant="link">
+            <Menu />
+          </Button>
+        </DialogTrigger>
+
+        <DialogContent className="top-0 right-0 bottom-0 left-auto h-full translate-none rounded-none">
+          <div className="flex flex-col">
+            <DialogHeader className="mb-4 text-left">
+              <DialogTitle>{t("logo")}</DialogTitle>
+            </DialogHeader>
+
+            <div className="divide-y-2 divide-gray-100">
+              <SignedIn>
+                {linksList.map(link => (
+                  <div key={link.name}>
+                    <DialogClose asChild>
+                      <Link href={link.url} className="block py-2">
+                        {link.name}
+                      </Link>
+                    </DialogClose>
+                  </div>
+                ))}
+              </SignedIn>
+              <SignedOut>
+                <SignInButton>
+                  <DialogClose asChild>
+                    <a className="block py-2">{t("signin")}</a>
+                  </DialogClose>
+                </SignInButton>
+                <SignUpButton>
+                  <DialogClose asChild>
+                    <a className="block py-2">{t("signup")}</a>
+                  </DialogClose>
+                </SignUpButton>
+              </SignedOut>
+            </div>
+
+            <div className="mt-auto">
+              <SignedOut>
+                <div className="flex items-center gap-2">
+                  <div className="border-primary h-6 border-l-2"></div>
+                </div>
+              </SignedOut>
+              <SignedIn>
+                <div className="size-7">
+                  <UserButton />
+                </div>
+              </SignedIn>
+            </div>
+          </div>
+        </DialogContent>
+      </div>
+    </Dialog>
   );
 }
 
