3a2b8ba9b4e30954776d52e0328ebdc715fdf481
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-13 01:25:54 +0400

    Update grid

--- app/components/price-list/price-list-goods.tsx
+++ app/components/price-list/price-list-goods.tsx
@@ -25,7 +25,7 @@ function PriceListGoods({ item, status, diff, favorites, isUserLoggedIn }: Price
     <div
       data-testid="pricelist-goods"
       className={clsx(
-        "grid gap-x-4 gap-y-1 py-4 [grid-template-areas:'image_prices'_'image_diff'_'image_store'_'description_description'] md:[grid-template-areas:'image_description_prices'_'image_description_diff'_'image_description_store'] lg:items-center lg:py-1 lg:[grid-template-areas:'image_description_diff_prices_store_favorites']",
+        "grid gap-x-4 gap-y-1 py-4 [grid-template-areas:'image_prices'_'image_diff'_'image_store'_'description_description'] md:[grid-template-areas:'image_description_prices'_'image_description_diff'_'image_description_store'] lg:[grid-template-columns:auto_1fr_auto_140px_130px_24px] lg:items-center lg:py-1 lg:[grid-template-areas:'image_description_diff_prices_store_favorites']",
         {
           "opacity-40": status?.deleted
         }
