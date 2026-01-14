b46deb2c647202beb8a364890eb2b358bb3e1a6f
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-14 14:19:04 +0400

    Add empty favorites

---
+++ app/components/favorites/favorites-empty.stories.tsx
@@ -0,0 +1,22 @@
+import { mockFavorites } from "./__mocks__/favories";
+import { FavoritesEmpty } from "./favorites-empty";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof FavoritesEmpty> = {
+  title: "Components/Favorites/FavoritesEmpty",
+  component: FavoritesEmpty,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "padded"
+  },
+  argTypes: {
+    favorites: { control: "object" },
+    shownBoughtFavorites: { control: "boolean" }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof FavoritesEmpty>;
+
+export const Default: Story = {};
