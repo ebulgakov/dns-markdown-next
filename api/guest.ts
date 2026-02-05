0ec125663b89750d910e8baf2e9d017643b2ec5c
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-05 22:36:26 +0400

    Replace with redis

--- api/guest.ts
+++ api/guest.ts
@@ -1,52 +1,16 @@
+"use server";
+
+import { Redis } from "@upstash/redis";
 import { cookies } from "next/headers";
-import { z } from "zod";
 
-import { Favorite, User } from "@/types/user";
+import { Goods } from "@/types/pricelist";
 
-import type { SectionsResponse } from "@/api/user";
+import type { FavoritesResponse, FavoritesStatusResponse, SectionsResponse } from "@/api/user";
+import type { Favorite, User } from "@/types/user";
 
-const goodsSchema = z.object({
-  _id: z.string(),
-  title: z.string(),
-  link: z.string(),
-  description: z.string(),
-  reasons: z.array(
-    z.object({
-      _id: z.string(),
-      label: z.string(),
-      text: z.string()
-    })
-  ),
-  priceOld: z.string(),
-  price: z.string(),
-  profit: z.string(),
-  code: z.string(),
-  image: z.string(),
-  available: z.string(),
-  city: z.string().optional()
-});
-
-const guestSchema = z.object({
-  userId: z.string(),
-  email: z.string().email(),
-  city: z.string(),
-  favorites: z.array(
-    z.object({
-      item: goodsSchema,
-      status: z.object({
-        deleted: z.boolean(),
-        createdAt: z.string()
-      })
-    })
-  ),
-  hiddenSections: z.array(z.string()),
-  favoriteSections: z.array(z.string()),
-  notifications: z.object({
-    updates: z.object({
-      enabled: z.boolean()
-    })
-  }),
-  shownBoughtFavorites: z.boolean()
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
 });
 
 export const getGuest = async (): Promise<User> => {
@@ -63,26 +27,32 @@ export const getGuest = async (): Promise<User> => {
   };
 
   const cookieStore = await cookies();
-  const guestSource = cookieStore.get("guest");
+  const guestId = cookieStore.get("guestId")?.value;
 
-  if (!guestSource) {
-    console.log("No guest cookie found, returning default guest.");
+  if (guestId) {
+    const data = await redis.get<User>(`guest:${guestId}`);
+    if (data) {
+      return data;
+    } else {
+      return defaultGuest;
+    }
+  } else {
     return defaultGuest;
   }
-
-  const validationResult = guestSchema.safeParse(JSON.parse(guestSource.value));
-
-  if (!validationResult.success) {
-    console.log("Guest cookie validation failed, returning default guest.", validationResult.error);
-    return defaultGuest;
-  }
-
-  return validationResult.data as unknown as User;
 };
 
 export const setGuest = async (guest: User) => {
   const cookieStore = await cookies();
-  cookieStore.set("guest", JSON.stringify(guest));
+  const guestId = cookieStore.get("guestId")?.value;
+
+  if (guestId) {
+    await redis.set(`guest:${guestId}`, guest);
+  } else {
+    const newGuestId = crypto.randomUUID();
+    cookieStore.set("guestId", newGuestId);
+
+    await redis.set(`guest:${newGuestId}`, guest);
+  }
 };
 
 export const addToHiddenSections = async (title: string): Promise<SectionsResponse> => {
@@ -133,7 +103,7 @@ export const removeFromFavoriteSections = async (title: string) => {
   };
 };
 
-export const addToFavorites = async (product: z.infer<typeof goodsSchema>) => {
+export const addToFavorites = async (product: Goods): Promise<FavoritesResponse> => {
   const guest = await getGuest();
   if (!guest.favorites.some(fav => fav.item.link === product.link)) {
     const newFavorite: Favorite = {
@@ -144,31 +114,28 @@ export const addToFavorites = async (product: z.infer<typeof goodsSchema>) => {
 
     guest.favorites.push(newFavorite);
     await setGuest(guest);
-
-    return {
-      message: "OK",
-      sections: guest.favorites
-    };
   }
+
+  return {
+    message: "OK",
+    favorites: guest.favorites
+  };
 };
 
-export const removeFromFavorites = async (link: string) => {
+export const removeFromFavorites = async (link: string): Promise<FavoritesResponse> => {
   const guest = await getGuest();
   guest.favorites = guest.favorites.filter(fav => fav.item.link !== link);
   await setGuest(guest);
 
   return {
     message: "OK",
-    sections: guest.favorites
+    favorites: guest.favorites
   };
 };
 
 export const toggleShownBoughtFavorites = async (
   shownBoughtFavorites: boolean
-): Promise<{
-  message: string;
-  shownBoughtFavorites: boolean;
-}> => {
+): Promise<FavoritesStatusResponse> => {
   const guest = await getGuest();
   guest.shownBoughtFavorites = shownBoughtFavorites;
   await setGuest(guest);
