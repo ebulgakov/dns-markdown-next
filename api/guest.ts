485d16306634fbc866fb158c80db3a9bc8cb77f2
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-05 13:24:06 +0400

    Add guest api placeholder

---
+++ api/guest.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import type { Goods } from "@/types/pricelist";
+import type { UserNotifications } from "@/types/user";
+
+export const postUpdateUserNotifications = async (notifications: UserNotifications) => {};
+export const postToggleFavoriteShownBought = async (shownBoughtFavorites: boolean) => {};
+export const postAddToFavorites = async (product: Goods) => {};
+export const postRemoveFromFavorites = async (link: string) => {};
+export const postAddToHiddenSections = async (title: string) => {};
+export const postRemoveFromHiddenSections = async (title: string) => {};
+export const postAddToFavoriteSections = async (title: string) => {};
+export const postRemoveFromFavoriteSection = async (title: string) => {};
+export const getUser = async () => {};
