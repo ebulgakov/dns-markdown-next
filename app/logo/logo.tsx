4b9d2b25a72cd4a5025ac27bfd55cc8b381e0fbb
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-07 16:45:21 +0400

    Add dropdown

--- app/logo/logo.tsx
+++ app/logo/logo.tsx
@@ -4,6 +4,14 @@ import { useTranslations } from "next-intl";
 import { useContext } from "react";
 
 import { Button } from "@/app/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuCheckboxItem
+} from "@/app/components/ui/dropdown-menu";
 import { UserContext } from "@/app/contexts/user-context";
 import { sendGAEvent } from "@/app/lib/sendGAEvent";
 
@@ -23,16 +31,35 @@ function Logo() {
 
   return (
     <div>
-      <Button asChild variant="link">
+      <Button asChild variant="link" className="cursor-pointer">
         <Link href="/" onClick={() => handleSendGAEvent({ name: "home", url: "/" })}>
           {t("logo")}
         </Link>
       </Button>
 
-      <Button variant="link" className="-ml-4 font-bold">
-        {city ? cities(city) : ""}
-        <ChevronDown className="text-secondary -m-1 size-5" />
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="link" className="-ml-4 cursor-pointer font-bold">
+            {city ? cities(city) : ""}
+            <ChevronDown className="text-secondary -m-1 size-5" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40">
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>{t("change_city")}</DropdownMenuLabel>
+            <DropdownMenuCheckboxItem checked={city === "samara"} onCheckedChange={() => {}}>
+              {cities("samara")}
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+              checked={city === "moscow"}
+              onCheckedChange={() => {}}
+              disabled
+            >
+              {cities("moscow")}
+            </DropdownMenuCheckboxItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
