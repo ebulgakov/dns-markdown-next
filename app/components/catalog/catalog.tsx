6234618dd5389a356e92c3edc36469e4ccdeef8e
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-08 23:19:20 +0400

    Detach Virtualization setup

--- app/components/catalog/catalog.tsx
+++ app/components/catalog/catalog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useWindowVirtualizer, VirtualItem } from "@tanstack/react-virtual";
 import { useDebounce } from "@uidotdev/usehooks";
 import { useEffect, useRef, useState } from "react";
 
@@ -8,6 +7,7 @@ import { CatalogFavoritesEmptyAlert } from "@/app/components/catalog/catalog-fav
 import { getCurrentCatalogTitle } from "@/app/components/catalog/helpers/get-current-catalog-title";
 import { ProductCard } from "@/app/components/product-card";
 import { Title } from "@/app/components/ui/title";
+import { useCatalogVirtualizer } from "@/app/hooks/use-catalog-virtualizer";
 import { useFilteredGoods } from "@/app/hooks/use-filtered-goods";
 import { cn } from "@/app/lib/utils";
 import { useSearchStore } from "@/app/stores/search-store";
@@ -58,67 +58,9 @@ function Catalog({
 
   // Virtualization setup
   const listRef = useRef<HTMLDivElement>(null);
-  const virtualizer = useWindowVirtualizer({
-    count: flattenList.length,
-    estimateSize: index => {
-      if (flattenList[index].type === "header") return 60;
-      return 220;
-    },
-    getItemKey: index => {
-      const item = flattenList[index];
-      if (!item) return index;
-      if (item.type === "goods") return (item as VisualizationGoods)._id;
-      if (item.type === "header") return `header-${(item as VisualizationHeader).title}`;
-      if (item.type === "title") return `title-${(item as VisualizationSectionTitle).category}`;
-      return index;
-    },
-    overscan: 5,
-    scrollMargin: 100, // Equal -mt-25 -> 25 * 4. I need it for correct work of sticky header overlapping
-    initialOffset: 0
-  });
+  const virtualizer = useCatalogVirtualizer({ flattenList, variant, listRef });
   const virtualItems = virtualizer.getVirtualItems();
-
   const currentTitle = getCurrentCatalogTitle(virtualItems, flattenList, flattenTitles);
-
-  useEffect(() => {
-    const handleHashScroll = () => {
-      const hash = window.location.hash;
-      if (!hash) return;
-
-      const title = decodeURIComponent(hash.slice(1));
-
-      const index = flattenList.findIndex(item => {
-        const header = item as VisualizationHeader;
-        return header.type === "header" && header.title === title;
-      });
-
-      if (index !== -1) {
-        const foundList = virtualizer.getOffsetForIndex(index, "start");
-        if (foundList == null) return;
-
-        const listOffset = listRef.current?.offsetTop ?? 0;
-        const navHeightStr = getComputedStyle(document.documentElement).getPropertyValue(
-          "--nav-bar-height"
-        );
-        let navHeight = parseInt(navHeightStr) || 56; // height of navbar + search bar
-
-        if (["default", "archive"].includes(variant)) {
-          navHeight *= 2;
-        }
-
-        window.scrollTo({ top: foundList[0] + listOffset - navHeight + 10 }); // Additional 10px for better visibility of the header
-        history.pushState(null, document.title, window.location.pathname + window.location.search);
-      }
-    };
-
-    const timeoutId = setTimeout(handleHashScroll, 100); // Delay to ensure the page has rendered and virtualizer has calculated item positions
-    window.addEventListener("hashchange", handleHashScroll);
-
-    return () => {
-      window.removeEventListener("hashchange", handleHashScroll);
-      clearTimeout(timeoutId);
-    };
-  }, [virtualizer, flattenList, variant]);
 
   useEffect(() => {
     setScrollHeight(virtualizer.getTotalSize());
