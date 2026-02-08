73e2f8b04c46e49b6117278c8db71f39194e65bc
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-08 19:08:42 +0400

    Fix bug with wrong title position

--- app/components/catalog/catalog-favorites-empty-alert.tsx
+++ app/components/catalog/catalog-favorites-empty-alert.tsx
@@ -2,15 +2,13 @@ import { Alert, AlertDescription, AlertTitle } from "@/app/components/ui/alert";
 
 function CatalogFavoritesEmptyAlert() {
   return (
-    <div className="mb-10">
-      <Alert variant="success">
-        <AlertTitle>У вас нет избранных категорий</AlertTitle>
-        <AlertDescription>
-          Добавьте избранные категории с помощью сердечка напротив заголовка и они всегда будут
-          закреплены вверху списка
-        </AlertDescription>
-      </Alert>
-    </div>
+    <Alert variant="success">
+      <AlertTitle>У вас нет избранных категорий</AlertTitle>
+      <AlertDescription>
+        Добавьте избранные категории с помощью сердечка напротив заголовка и они всегда будут
+        закреплены вверху списка
+      </AlertDescription>
+    </Alert>
   );
 }
 
