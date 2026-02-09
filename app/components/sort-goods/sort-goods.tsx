426bf822619d7c524d2d07e60742a5fb1c09fdc3
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-09 09:39:44 +0400

    Add more tooltips

--- app/components/sort-goods/sort-goods.tsx
+++ app/components/sort-goods/sort-goods.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Info } from "lucide-react";
+
 import {
   Select,
   SelectContent,
@@ -8,6 +10,7 @@ import {
   SelectTrigger,
   SelectValue
 } from "@/app/components/ui/select";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/app/components/ui/tooltip";
 import { useSortGoodsStore } from "@/app/stores/sort-goods-store";
 
 import type { SortGoodsOptions } from "@/types/common";
@@ -17,20 +20,41 @@ function SortGoods() {
   const sortGoods = useSortGoodsStore(state => state.sortGoods);
 
   return (
-    <Select onValueChange={(e: SortGoodsOptions) => onChange(e)} value={sortGoods}>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Сортировка" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Сортировка</SelectLabel>
-          <SelectItem value="default">По умолчанию</SelectItem>
-          <SelectItem value="price">По цене</SelectItem>
-          <SelectItem value="discount">По размеру скидки</SelectItem>
-          <SelectItem value="profit">По максимальной выгоде</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div className="flex gap-1">
+      <Tooltip>
+        <TooltipTrigger>
+          <Info />
+        </TooltipTrigger>
+        <TooltipContent side="left">
+          <p>
+            <b>По умолчанию</b>&nbsp;&mdash; как товары представлены на сайте
+          </p>
+          <p>
+            <b>По цене</b>&nbsp;&mdash; от самого дешёвого до дорогого товара
+          </p>
+          <p>
+            <b>По размеру скидки</b>&nbsp;&mdash; по размеру скидки в %
+          </p>
+          <p>
+            <b>По максимальной выгоде</b>&nbsp;&mdash; по максимальной выгоде в рублях
+          </p>
+        </TooltipContent>
+      </Tooltip>
+      <Select onValueChange={(e: SortGoodsOptions) => onChange(e)} value={sortGoods}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Сортировка" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Сортировка</SelectLabel>
+            <SelectItem value="default">По умолчанию</SelectItem>
+            <SelectItem value="price">По цене</SelectItem>
+            <SelectItem value="discount">По размеру скидки</SelectItem>
+            <SelectItem value="profit">По максимальной выгоде</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   );
 }
 
